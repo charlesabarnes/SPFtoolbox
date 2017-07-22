5393cf89206236e0359e1cee327dddc4f7b33107
Michael Dill <MichaelDill.stl@gmail.com>
2017-07-22 15:12:32 -0500

    Obtain domain from request url

--- getDns.php
+++ getDns.php
@@ -5,17 +5,20 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header('Content-Type: application/json');
 
+$q = $_REQUEST["q"];
+
+
 class DNS{
     public function getDNS($hostname){
     $response = dns_get_record ($hostname , DNS_TXT);
         foreach($response as $result){
-            print_r(json_encode($result, JSON_PRETTY_PRINT));
+            print_r(json_encode($result, JSON_PRETTY_PRINT));;
             
         }       
     }
 }
 $bullhorn = new DNS;
-$bullhorn->getDNS($_GET['domain']);
+$bullhorn->getDNS($q);
 
 
 ?>
