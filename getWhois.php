98202a79c5fa6824b0834b61dcdcd2dfb85d24fe
Charles Barnes <charlesabarnesjr@gmail.com>
2017-07-23 16:12:57 -0500

    update to to many

--- getWhois.php
+++ getWhois.php
@@ -10,5 +10,7 @@ include_once('phpwhois/whois.utils.php');
 $whois = new Whois();
 $query = $_GET['domain'];
 $result = $whois->lookup($query);
+echo "[\n";
 echo json_encode($result, JSON_PRETTY_PRINT);
+echo "\n]";
 ?>
