50a18b646f7cc1f4b7e5a39a067c11ebed0fac83
frju365 <win10@tutanota.com>
2018-04-22 11:01:24 +0200

    [fix] .out

--- getPort.php
+++ getPort.php
@@ -5,28 +5,30 @@ header("Pragma: no-cache");
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");        
         
-class Port {
-    function getPort($hostname) {
+function getPort($hostname) {
       $i = 0;
       $ports = array(22 => "SSH", 25 => "SMTP", 53 => "DNS", 80 => "HTTP", 443 => "HTTPS", 465 => "SMTPS", 587 => "IMAP", 993 => "IMAPS", 5222 => "XMPP Jabber", 5269 => "Server to server Jabber");
-        foreach ($ports as $port => $protocole) {
-            $fp = @fsockopen($hostname, $port, $errno, $errstr, 5);
-            if (!$fp) {
-                echo $protocole, " : Port ", $port, " is closed" . "\n";
-            } else {
-                echo $protocole, " : Port ", $port, " is open" . "\n";
-                if ($fp)
-                {
-                    @fclose($x); //close connection
-                }
-            }
-            $i++;
-        }
-    }
+       foreach ($ports as $port => $protocole) {
+       $fp = @fsockopen($hostname, $port, $errno, $errstr, 5);
+       if (!$fp) {
+           echo $protocole, " : Port ", $port, " is closed" . "\n";
+      } else {
+          echo $protocole, " : Port ", $port, " is open" . "\n";
+          if ($fp)
+          {
+              @fclose($x); //close connection
+          }
+      }
+      $i++;
+   }
 }
 
-$object = new Port;
-$object->getPort($_GET['domain']);
-
-echo json_encode($result['rawdata'], JSON_PRETTY_PRINT);
+$domain = $_GET['domain']
+if(isset($_GET['domain']) && $_GET['domain']!=null){
+   if(filter_var($domain,FILTER_VALIDATE_DOMAIN) or filter_var($domain,FILTER_VALIDATE_IP)){
+        getPort($domain);
+   }else{
+        echo "Please enter a valid domain/IP";
+   }
+}
 ?>
