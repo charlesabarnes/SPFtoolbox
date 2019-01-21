d2278342520a654d2d8ac119f4395755ee07b65b
Charles Barnes <charlesabarnesjr@gmail.com>
2019-01-21 14:13:55 -0600

    remove unecessary check

--- operations/index.php
+++ operations/index.php
@@ -6,57 +6,54 @@ header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 
 $domain = $_GET['domain'];
-if(isset($domain) && $domain!=null){
+if(isset($domain) && $domain !=null){
 
-  if(filter_var($domain,FILTER_VALIDATE_DOMAIN) or filter_var($domain,FILTER_VALIDATE_IP)){
-    switch ($_GET['request']) {
-      case 'a':
-        include_once('./A.php');
-        $object = new A;
-        break;
-      case 'aaaa':
-        include_once('./AAAA.php');
-        $object = new AAAA;
-        break;
-      case 'all':
-        include_once('./All.php');
-        $object = new All;
-        break;
-      case 'blacklist':
-        include_once('./Blacklist.php');
-        $object = new Blacklist;
-        break;
-      case 'hinfo':
-        include_once('./Hinfo.php');
-        $object = new Hinfo;
-        break;
-      case 'mx':
-        include_once('./Mx.php');
-        $object = new Mx;
-        break;
-      case 'port':
-        include_once('./Port.php');
-        $object = new Port($_GET['port']);
-        break;
-      case 'reverseLookup':
-        include_once('./ReverseLookup.php');
-        $object = new ReverseLookup;
-        break;
-      case 'txt':
-        include_once('./Txt.php');
-        $object = new AAAA;
-        break;
-      case 'whois':
-        include_once('./Whois.php');
-        $object = new WhoisOutput;
-        break;
-      default:
-        echo '[{"error": "Please check a valid DNS type"}]';
-        break;
-    }
-    print_r($object->getOutput($domain));
-  } else{
-  echo '[{"error": "Please enter a valid domain/IP"}]';
+  switch ($_GET['request']) {
+    case 'a':
+      include_once('./A.php');
+      $object = new A;
+      break;
+    case 'aaaa':
+      include_once('./AAAA.php');
+      $object = new AAAA;
+      break;
+    case 'all':
+      include_once('./All.php');
+      $object = new All;
+      break;
+    case 'blacklist':
+      include_once('./Blacklist.php');
+      $object = new Blacklist;
+      break;
+    case 'hinfo':
+      include_once('./Hinfo.php');
+      $object = new Hinfo;
+      break;
+    case 'mx':
+      include_once('./Mx.php');
+      $object = new Mx;
+      break;
+    case 'port':
+      include_once('./Port.php');
+      $object = new Port($_GET['port']);
+      break;
+    case 'reverseLookup':
+      include_once('./ReverseLookup.php');
+      $object = new ReverseLookup;
+      break;
+    case 'txt':
+      include_once('./Txt.php');
+      $object = new AAAA;
+      break;
+    case 'whois':
+      include_once('./Whois.php');
+      $object = new WhoisOutput;
+      break;
+    default:
+      echo '[{"error": "Please check a valid DNS type"}]';
+      break;
   }
+  print_r($object->getOutput($domain));
+
 }
 ?>
