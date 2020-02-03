5a1757e2eec7f390bdd393a1659bc6d56d47373b
root <root@eriklentz.com>
2020-02-03 09:57:42 -0600

    DMARC functionality cleanup

--- operations/Dmarc.php
+++ operations/Dmarc.php
@@ -1,7 +1,9 @@
 <?php
 include_once('./OperationInterface.php');
 $dmarc = "_dmarc.";
-$dmarchostname = $dmarc . $hostname;
+$dmarchostname = $dmarc . $domain;
+$hostname = $dmarchostname;
+$domain = $dmarchostname;
 class Dmarc implements OperationInterface{
     public function getOutput($hostname){
         $response = dns_get_record ($hostname , DNS_TXT);  
