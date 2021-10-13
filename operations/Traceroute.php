3289aa443fc4cd85005adb99314eca52abdd96ce
Charles Barnes <charles@charlesabarnes.com>
2021-10-13 15:23:07 -0500

    error log for debug

--- operations/Traceroute.php
+++ operations/Traceroute.php
@@ -10,6 +10,7 @@ class Traceroute implements OperationInterface{
     private function getTraceroute($host) {
         exec('traceroute '.$host.' 2>&1', $out, $code);
         if ($code) {
+            error_log($out);
             return [];
         }
         return [$out];
