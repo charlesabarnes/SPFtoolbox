624be659b4e9aa63e109a789d6617622e79d5bfa
Charles Barnes <charles@charlesabarnes.com>
2021-10-12 19:40:02 -0500

    change directory

--- operations/Ping.php
+++ operations/Ping.php
@@ -8,7 +8,7 @@ class Ping implements OperationInterface{
       if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
         exec("ping -n 4 " . $hostname, $output, $status);
       } else {
-        exec("ping -c 4 " . $hostname, $output, $status);
+        exec("/bin/ping -c 4 " . $hostname, $output, $status);
       }
       error_log($output);
       error_log($hostname);
