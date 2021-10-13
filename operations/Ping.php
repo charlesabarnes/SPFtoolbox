96f03247d49477c9bb32f297959ebcfb362745e0
Charles Barnes <charles@charlesabarnes.com>
2021-10-12 19:22:52 -0500

    use proper variable

--- operations/Ping.php
+++ operations/Ping.php
@@ -2,12 +2,12 @@
 include_once('./OperationInterface.php');
 
 class Ping implements OperationInterface{
-  
+
     public function getOutput($hostname) {
       if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-        exec("ping -n 4 " . $host, $output, $status);
+        exec("ping -n 4 " . $hostname, $output, $status);
       } else {
-        exec("ping -c 4 " . $host, $output, $status);
+        exec("ping -c 4 " . $hostname, $output, $status);
       }
       return json_encode($output, JSON_PRETTY_PRINT);
     }
