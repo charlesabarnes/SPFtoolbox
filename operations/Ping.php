6ae2704eb10cce4961222f442309ee66cb24c062
Charles Barnes <charles@charlesabarnes.com>
2021-10-12 19:21:02 -0500

    format whitespace

--- operations/Ping.php
+++ operations/Ping.php
@@ -2,13 +2,14 @@
 include_once('./OperationInterface.php');
 
 class Ping implements OperationInterface{
-    public function getOutput($hostname){
+  
+    public function getOutput($hostname) {
       if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
         exec("ping -n 4 " . $host, $output, $status);
       } else {
         exec("ping -c 4 " . $host, $output, $status);
       }
-        return json_encode($output, JSON_PRETTY_PRINT);
+      return json_encode($output, JSON_PRETTY_PRINT);
     }
 }
 ?>
